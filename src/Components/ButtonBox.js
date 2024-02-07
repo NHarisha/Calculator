36a4d236544cff60251da0ab07f183b6da73b9fa
N.Harisha <nharisha552@gmail.com>
2024-02-07 21:43:11 +0530

    Project Calculator

---
+++ src/Components/ButtonBox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Buttonbox.css"
+
+const ButtonBox = ({children}) => {
+  return (
+    <div className="Buttonbox">{children}</div>
+  )
+}
+
+export default ButtonBox
